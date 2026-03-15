1f854c4739cdf9e96efa160eca228016a082fab7
Jayden Huang <Jayden.Huang+cba@cba.com.au>
2026-03-16 10:17:28 +1100

    Added store helper

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 // STORE = the main container holding all your app's state. 
 // It allows you to access the state, dispatch actions, and subscribe to changes. 
 // In Redux Toolkit, we use configureStore to create the store and combine our reducers.
+
+// SLICES = seperate sections of the store, like database tables.
 import { configureStore } from '@reduxjs/toolkit';
 import playersReducer from './playersSlice';
 import squadReducer from './squadSlice';
